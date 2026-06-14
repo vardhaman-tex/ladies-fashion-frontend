bd1445b8130411fc714db61c21a21e0897c72b20
dicusa <jain.yash1909@gmail.com>
2026-06-14 18:37:48 +0530

    fix availableQty optional in CartItemData

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -11,7 +11,7 @@ export interface CartItemData {
   size: string | null;
   color: string | null;
   lineTotal: number;
-  availableQty: number;
+  availableQty?: number;
 }
 
 export interface CartData {
